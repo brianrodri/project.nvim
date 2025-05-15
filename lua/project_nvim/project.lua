a93896af91d2467e058ec12a6beef99f80a31623
Brian Rodriguez <brian@brianrodri.com>
2025-05-15 01:11:00 -0400

    fix: LSP DetectionMethod should not ignore `exclude_dirs` (#5)
    * fix: LSP DetectionMethod should not ignore `exclude_dirs`
    
    * refactor: check if buffer is attached first

--- lua/project_nvim/project.lua
+++ lua/project_nvim/project.lua
@@ -14,8 +14,9 @@ M.last_project = nil
 ---@param buffer_id integer
 ---@return boolean
 local function is_lsp_with_root_dir(lsp_client, buffer_id)
-  return lsp_client.config.root_dir ~= nil
-    and lsp_client.attached_buffers[buffer_id]
+  return lsp_client.attached_buffers[buffer_id]
+    and lsp_client.config.root_dir ~= nil
+    and not path.is_excluded(lsp_client.config.root_dir)
     and not vim.tbl_contains(config.options.ignore_lsp, lsp_client.name)
 end
 
