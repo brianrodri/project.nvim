5c0eb54fef2f8ae1b2c17e76fa3dd24666299252
Brian Rodriguez <brian@brianrodri.com>
2025-05-17 08:40:33 -0400

    feat: add data_dir option to user config

--- lua/projects/types.lua
+++ lua/projects/types.lua
@@ -1,10 +1,18 @@
 -- TODO: Define types.
---
+
+---@alias projects.PathLike (string | projects.Path)
+
 ---@class projects.UserConfig
 ---
+--- Determines where the plugin stores its persistent state.
+---@field data_dir? string|fun(): string
+
 ---@class projects.AddProjectOpts
+
 ---@class projects.DeleteProjectOpts
+
 ---@class projects.GetRecentProjectsOpts
+
 ---@class projects.EnterProjectDirectoryOpts
----
+
 ---@class projects.State
