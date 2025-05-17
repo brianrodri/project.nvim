bb57ca0cec71bb7af13ecf2f757b39a056cc2a04
Brian Rodriguez <brian@brianrodri.com>
2025-05-17 07:10:55 -0400

    feat: add Path adapter class for the built-in filesystem functions (#10)
    * chore: setup api facade with unimplemented functions
    
    * feat: add a Path class wrapper around built-in filesystem functions
    
    * chore: resolve merge conflicts

--- lua/projects/errors.lua
+++ lua/projects/errors.lua
@@ -1,21 +1,24 @@
 local M = {}
 
---- Raises a helpful error with debug info about the call responsible.
+--- Returns a helpful error message with debug info about the function call responsible.
 ---
 ---@param err unknown The error object.
----@param func_name string The name of the function that raised the error.
+---@param func_name string The name of the function that caused the error.
 ---@param ... any The arguments passed to the function.
----@return unknown ... This function never returns, but the annotation convinces LuaLS that it does.
-function M.call_failure(err, func_name, ...)
+---@return string call_error A helpful error with debug info about the call responsible.
+function M.format_call_error(err, func_name, ...)
   local debug_args = vim.fn.join(vim.tbl_map(vim.inspect, { ... }), ", ")
-  error(string.format("%s(%s) error: %s", func_name, debug_args, tostring(err)))
+  return string.format("%s(%s) error: %s", func_name, debug_args, tostring(err))
 end
 
---- Raises a helpful "not implemented" error.
+--- Terminates the last protected call with a helpful "not implemented" error.
 ---
----@param func_name string The name of the function that raised the error.
+---@param func_name string The name of the function that caused the error.
 ---@param ... any The arguments passed to the function.
 ---@return unknown ... This function never returns, but the annotation convinces LuaLS that it does.
-M.TODO = function(func_name, ...) M.call_failure("not implemented", func_name, ...) end
+M.TODO = function(func_name, ...)
+  -- NOTE: Intentionally different definition style: I don't want searches to consider this line as a TODO.
+  error(M.format_call_error("not implemented", func_name, ...))
+end
 
 return M
