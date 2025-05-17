cd133495d53180586cdd47c9a7bcea80a8eb70e3
Brian Rodriguez <brian@brianrodri.com>
2025-05-17 15:38:38 -0400

    feat: use a new table to hold resolved values

--- lua/projects/config.lua
+++ lua/projects/config.lua
@@ -30,14 +30,15 @@ local FIELD_RESOLVERS = {
 ---@return projects.ResolvedConfig
 function M.resolve_opts(opts)
   opts = vim.tbl_deep_extend("force", vim.deepcopy(DEFAULT_CONFIG), opts or {})
+  local resolved = {}
   local resolve_errors = {}
   for field, resolver in pairs(FIELD_RESOLVERS) do
-    local ok, value = pcall(resolver, opts, opts)
+    local ok, value = pcall(resolver, opts, resolved)
     if not ok then table.insert(resolve_errors, string.format('invalid "%s": %s', field, vim.inspect(value))) end
   end
   assert(#resolve_errors == 0, fmt.call_error(errors.join(resolve_errors), "resolve_opts", opts))
-  ---@cast opts projects.ResolvedConfig
-  return opts
+  ---@cast resolved projects.ResolvedConfig
+  return resolved
 end
 
 return M
