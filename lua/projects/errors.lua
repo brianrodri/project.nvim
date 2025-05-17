9dab162c51c96de7762b6c53b07bf1cc55369cd0
Brian Rodriguez <brian@brianrodri.com>
2025-05-17 03:00:39 -0400

    docs: fix typos and add motivations

--- lua/projects/errors.lua
+++ lua/projects/errors.lua
@@ -2,10 +2,13 @@ local M = {}
 
 --- Raises an error with debug information about the function call.
 ---
----@param err any The error object.
+---@param err unknown The error object.
+---
 ---@param func_name string The name of the function that raised the error.
+---
 ---@param ... any The arguments passed to the function.
----@return unknown ...
+---
+---@never
 function M.call_failure(err, func_name, ...)
   local debug_args = vim.fn.join(vim.tbl_map(vim.inspect, { ... }), ", ")
   error(string.format("%s(%s) error: %s", func_name, debug_args, tostring(err)))
@@ -14,8 +17,12 @@ end
 --- Raises a helpful "not implemented" error.
 ---
 ---@param func_name string The name of the function that raised the error.
----@param ... unknown The arguments passed to the function.
----@return unknown ...
+---
+---@param ... any The arguments passed to the function.
+---
+---@never This function never returns normally
+---
+---@return unknown ... This annotation allows the function to be used as any return value.
 M.TODO = function(func_name, ...) M.call_failure("not implemented", func_name, ...) end
 
 return M
