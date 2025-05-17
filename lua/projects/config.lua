bc474755877bf7ac01766838f7f9d1600221b038
Brian Rodriguez <brian@brianrodri.com>
2025-05-17 01:27:19 -0400

    feat: setup api facade with unimplemented functions

---
+++ lua/projects/config.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+---@type projects.UserConfig
+local DEFAULT_CONFIG = {}
+
+---@param opts? projects.UserConfig
+---@return projects.UserConfig
+function M.resolve_opts(opts) return vim.tbl_deep_extend("force", vim.deepcopy(DEFAULT_CONFIG), opts) end
+
+return M
